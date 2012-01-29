7edbb747b73074c0319cd81891293ef7837116ae
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2012-01-29 08:55:33 +0100

    enlarge water

--- NGJ2012/NGJ2012/WaterLayer.cs
+++ NGJ2012/NGJ2012/WaterLayer.cs
@@ -118,7 +118,7 @@ namespace NGJ2012
                 for (var substep = 0; substep < resolution && (x != parent.WorldWidthInBlocks || substep == 0); substep++)
                 {
                     var rx = (float)x + (float)substep * (1f / resolution);
-                    array[i++] = new VertexPositionColor(new Vector3(rx, 2, 0), Color.Black);
+                    array[i++] = new VertexPositionColor(new Vector3(rx, 5, 0), Color.Black);
 
                     float h = pos.Y;
                     //h += (float)Math.Sin(rx - gameTime.TotalGameTime.TotalSeconds * 2.1) * 0.1f - 0.1f;
